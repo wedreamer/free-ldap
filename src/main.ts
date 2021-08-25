74f633e6bf3dd83e205717a975dc844c9d545d15
shubuzuo <shubuzuo@gmail.com>
2021-08-25 10:51:10 +0800

    save

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
-let str = 'this is test ts and node env';
-str = '123';
-console.log('this is test ts and node env');
+import { createServer } from 'net';
+
+const netServer = createServer((socket) => {
+    // 'connection' 监听器。
+    console.log('client connected');
+    socket.on('end', () => {
+        console.log('client disconnected');
+    });
+    socket.write('hello\r\n');
+    socket.pipe(socket);
+});
+
+netServer.on('error', (err) => {
+    throw err;
+});
+netServer.listen(8124, () => {
+    console.log('server bound');
+});
